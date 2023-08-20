563c297a54d7ffb4ac6a93153b0265defc375841
uni <uniwuni@protonmail.com>
2023-08-20 18:30:05 +0200

    closure of bounded formulas

--- Arithmetics/QBFormulas.lean
+++ Arithmetics/QBFormulas.lean
@@ -1,8 +1,9 @@
 import Mathlib.ModelTheory.Order
+import Mathlib.ModelTheory.Satisfiability
 namespace FirstOrder.Language
 
 open FirstOrder
-variable {L} [IsOrdered L] {n : ℕ}
+variable {L} [IsOrdered L] {n : ℕ} {T : Theory L}
 --variable {l : ℕ} {φ ψ : L.BoundedFormula α l} {θ : L.BoundedFormula α l.succ}
 
 variable {v : α → M} {xs : Fin l → M}
@@ -28,6 +29,119 @@ theorem IsQB.not {φ : L.BoundedFormula α n} (h : IsQB φ) : IsQB φ.not :=
   h.imp isQB_bot
 
 
+variable (T : Theory L)
+open Theory
+structure QBEquiv (φ : L.BoundedFormula α n) :=
+  formula : L.BoundedFormula α n
+  isQB : IsQB formula
+  equiv : T.SemanticallyEquivalent φ formula
+
+theorem IsQB.isQBEquiv {φ : L.BoundedFormula α n} (h : IsQB φ) : QBEquiv T φ where
+  formula := φ
+  isQB := h
+  equiv := refl _
+
+section
+variable {φ ψ : L.BoundedFormula α n} (hφ : QBEquiv T φ) (hψ : QBEquiv T ψ)
+
+theorem QBEquiv.not : QBEquiv T φ.not where
+  formula := hφ.formula.not
+  isQB := hφ.isQB.not
+  equiv := SemanticallyEquivalent.not hφ.equiv
+
+@[simp]
+lemma QBEquiv.not_formula : (QBEquiv.not T hφ).formula = hφ.formula.not := by
+  rw[formula]
+  unfold not
+  simp
+
+
+theorem QBEquiv.imp : QBEquiv T (φ.imp ψ) where
+  formula := hφ.formula.imp hψ.formula
+  isQB := hφ.isQB.imp hψ.isQB
+  equiv := SemanticallyEquivalent.imp hφ.equiv hψ.equiv
+
+@[simp]
+lemma QBEquiv.imp_formula : (QBEquiv.imp T hφ hψ).formula = hφ.formula.imp hψ.formula := by
+  rw[formula]
+  unfold imp
+  simp
+
+theorem QBEquiv.or : QBEquiv T (φ ⊔ ψ) where
+  formula := hφ.formula.not.imp hψ.formula
+  isQB := hφ.isQB.not.imp hψ.isQB
+  equiv := by 
+    intro M v xs
+    have h1 := hφ.equiv
+    have h2 := hψ.equiv
+    simp only [realize_iff, realize_sup, realize_imp, realize_not]
+    have h3 := h1 M v xs
+    have h4 := h2 M v xs
+    simp only [realize_iff] at h3 h4
+    rw[h3, h4]
+    rw[or_iff_not_imp_left]
+
+@[simp]
+lemma QBEquiv.or_formula : (QBEquiv.or T hφ hψ).formula = hφ.formula.not.imp hψ.formula := by
+  rw[formula]
+  unfold or
+  simp
+
+theorem QBEquiv.and : QBEquiv T (φ ⊓ ψ) where
+  formula := (((hφ.not T).or T (hψ.not T)).not T).formula
+  isQB := (((hφ.not T).or T (hψ.not T)).not T).isQB
+  equiv := by
+    intro M v xs
+    have h1 := hφ.equiv
+    have h2 := hψ.equiv
+    simp only [realize_iff, realize_inf]
+    have h3 := h1 M v xs
+    have h4 := h2 M v xs
+    simp only [realize_iff] at h3 h4
+    rw[h3, h4]
+    simp
+    
+@[simp]
+lemma QBEquiv.and_formula : (QBEquiv.and T hφ hψ).formula = (((hφ.not T).or T (hψ.not T)).not T).formula := by
+  rw[formula]
+  unfold and
+  simp
+end
+section
+variable {φ ψ : L.BoundedFormula α (n + 1)} (hφ : QBEquiv T φ) (hψ : QBEquiv T ψ)
+lemma QBEquiv.bounded_forall (t : L.Term (α ⊕ Fin n)) : QBEquiv T (∀' ((&(↑ n)).le (t.relabel (Sum.map id Fin.castSucc)) ⟹ φ)) where
+  formula := (∀' ((&(↑ n)).le (t.relabel (Sum.map id Fin.castSucc)) ⟹ hφ.formula))
+  isQB := hφ.isQB.bounded_forall t
+  equiv := by
+    intro M v xs
+    simp only [Function.comp_apply, realize_iff, realize_all, realize_imp]
+    apply forall_congr'
+    intro a
+    have h1 := hφ.equiv
+    have h3 := h1 M v (Fin.snoc xs a)
+    simp only [realize_iff] at h3
+    rw[h3]
+
+lemma QBEquiv.bounded_exists (t : L.Term (α ⊕ Fin n)) : QBEquiv T (∃' ((&(↑ n)).le (t.relabel (Sum.map id Fin.castSucc)) ⊓ φ)) where
+  formula := (∀' ((&(↑ n)).le (t.relabel (Sum.map id Fin.castSucc)) ⟹ hφ.formula.not)).not
+  isQB := (hφ.isQB.not.bounded_forall t).not
+  equiv := by
+    intro M v xs
+    simp only [Function.comp_apply, realize_iff, realize_all, realize_imp]
+    unfold BoundedFormula.ex
+    apply not_congr
+    apply forall_congr'
+    intro a
+    have h1 := hφ.equiv
+    have h3 := h1 M v (Fin.snoc xs a)
+    simp only [realize_iff] at h3
+    simp only [realize_not, realize_inf, not_and, realize_imp]
+    rw[h3]
+
+
+
+
+end
   /-
 
 
